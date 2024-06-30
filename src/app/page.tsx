b2e8715347741a48aaf7be7ce9e91e742697bff9
tukwan <7630720+tukwan@users.noreply.github.com>
2024-06-30 12:17:24 +0700

    init mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,227 @@
-import Image from "next/image";
+import "./app.scss"
+import Image from "next/image"
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="page-container">
+      <section className="how-it-works section">
+        <header>
+          <h2 className="headline">How It Works</h2>
+          <h1 className="heading">Drive away with more than just a car</h1>
+        </header>
+
+        <nav className="steps">
+          <div className="step">
+            <a href="#find" className="nav-link">
+              <Image
+                src="icons/search.svg"
+                alt="Search icon"
+                width={20}
+                height={20}
+              />
+              Find a car
+            </a>
+          </div>
+          <div className="step">
+            <a href="#find" className="nav-link">
+              <Image
+                src="icons/contact.svg"
+                alt="Contact icon"
+                width={20}
+                height={20}
+              />
+              Contact us
+            </a>
+          </div>
+          <div className="step">
+            <a href="#find" className="nav-link">
+              <Image
+                src="icons/check.svg"
+                alt="Check icon"
+                width={20}
+                height={20}
+              />
+              Test drive
+            </a>
+          </div>
+          <div className="step">
+            <a href="#find" className="nav-link">
+              <Image
+                src="icons/bitcoin.svg"
+                alt="Pay icon"
+                width={20}
+                height={20}
+              />
+              Pay with crypto
+            </a>
+          </div>
+          <div className="step">
+            <a href="#find" className="nav-link">
+              <Image
+                src="icons/car.svg"
+                alt="Car icon"
+                width={20}
+                height={20}
+              />
+              Drive away
+            </a>
+          </div>
+        </nav>
+
+        <div className="process-container">
+          <div className="process-step">
+            <div className="step-number">1</div>
+            <p className="step-description">
+              Find your dream car. Explore the vast selection of over 20,000
+              cars on our listings page.
+            </p>
+          </div>
+          <div className="process-step">
+            <div className="step-number">2</div>
+            <p className="step-description">
+              Contact us via email, WhatsApp, or on the phone and have a
+              CryptoAutos representative run you through the buying process and
+              arranging a test drive. Once you’re happy, you can reserve your
+              vehicle.
+            </p>
+          </div>
+
+          <div className="process-step">
+            <div className="step-number">3</div>
+            <p className="step-description">
+              Process payment for the vehicle with your chosen cryptocurrency
+              and CryptoAutos will settle the invoice with the dealership.
+            </p>
+          </div>
+          <div className="process-step">
+            <div className="step-number">4</div>
+            <p className="step-description">
+              Collect your vehicle from the dealership for the final handover.
+            </p>
+          </div>
         </div>
-      </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
+        <div className="vision-container">
+          <p>
+            Our vision is to create a{" "}
+            <span className="highlight-text">
+              global network of hyper-personal crypto concierge services
+            </span>{" "}
+            that support our communities lifestyle in a transparent, trusted and
+            seamless way. With our combined expertise in both the automotive and
+            crypto industries we are creating a{" "}
+            <span className="highlight-text">true service of the future</span>.
           </p>
-        </a>
+        </div>
+      </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      <section className="results-so-far p-6 bg-white text-black section">
+        <header>
+          <h2 className="headline">Results so far</h2>
+          <h1 className="heading">Drive away with more than just a car</h1>
+        </header>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+        <div className="financial-container">
+          <div className="financial-card">
+            <h3 className="financial-value">$4.17T</h3>
+            <p className="financial-description">Automotive Market Cap</p>
+          </div>
+          <div className="financial-card">
+            <h3 className="financial-value">$53m</h3>
+            <p className="financial-description">Volume to date</p>
+          </div>
+          <div className="financial-card">
+            <h3 className="financial-value">$15.75m</h3>
+            <p className="financial-description">Transaction volume 2023</p>
+          </div>
+          <div className="financial-card">
+            <h3 className="financial-value">5.5%</h3>
+            <p className="financial-description">Our average margin</p>
+          </div>
+          <div className="financial-card">
+            <h3 className="financial-value">$1b</h3>
+            <p className="financial-description">
+              Crypto Automotive Market Cap
+            </p>
+          </div>
+          <div className="financial-card">
+            <h3 className="financial-value">$4.4m</h3>
+            <p className="financial-description">Highest transaction</p>
+            <p className="financial-tag">LA-FERRARI</p>
+          </div>
+          <div className="financial-card">
+            <h3 className="financial-value">$4.4m</h3>
+            <p className="financial-description">Highest transaction</p>
+            <p className="financial-tag">FIAT 500</p>
+          </div>
+        </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <div className="promo-card bg-black">
+          <div className="promo-content">
+            <Image
+              src="icons/coin.svg"
+              alt="AutoCoinCars icon"
+              width={55}
+              height={55}
+              className="promo-logo"
+            />
+            <Image
+              src="icons/logo.svg"
+              alt="AutoCoinCars icon"
+              width={217}
+              height={24}
+              className="promo-logo"
+            />
+          </div>
+        </div>
+
+        <div>
+          <h1 className="heading">
+            We recently acquired AutoCoin Cars to bolster our network and
+            offering
+          </h1>
+
+          <div className="feature-list">
+            <div className="feature-item">
+              <div className="feature-box">
+                <Image
+                  src="icons/tick.svg"
+                  alt="Tick icon"
+                  width={24}
+                  height={24}
+                />
+              </div>
+              <p className="feature-text">Access to over 17,000 Luxury Cars</p>
+            </div>
+            <div className="feature-item">
+              <div className="feature-box">
+                <Image
+                  src="icons/tick.svg"
+                  alt="Tick icon"
+                  width={24}
+                  height={24}
+                />
+              </div>
+              <p className="feature-text">
+                Global network of Car Dealers around the world
+              </p>
+            </div>
+            <div className="feature-item">
+              <div className="feature-box">
+                <Image
+                  src="icons/tick.svg"
+                  alt="Tick icon"
+                  width={24}
+                  height={24}
+                />
+              </div>
+              <p className="feature-text">
+                Annual Revenue of $5.6 Million per year.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
-  );
+  )
 }
